c6243187bc6fb401630c423b3824a32a7b4ef23b
Arathi Variar <arathivariar@gmail.com>
2024-05-14 11:00:13 +0000

    Added extra fields to Books and Profiles for data filtering

--- books/serializers.py
+++ books/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework import serializers
 from books.models import Book
+from likes.models import Like
 
 
 class BookSerializer(serializers.ModelSerializer):
@@ -7,6 +8,7 @@ class BookSerializer(serializers.ModelSerializer):
     is_owner = serializers.SerializerMethodField()
     profile_id = serializers.ReadOnlyField(source='owner.profile.id')
     profile_image = serializers.ReadOnlyField(source='owner.profile.image.url')
+    like_id = serializers.SerializerMethodField()
 
     def validate_image(self, value):
         if value.size > 2 * 1024 * 1024:
@@ -25,10 +27,20 @@ class BookSerializer(serializers.ModelSerializer):
         request = self.context['request']
         return request.user == obj.owner
 
+    def get_like_id(self, obj):
+        user = self.context['request'].user
+        if user.is_authenticated:
+            like = Like.objects.filter(
+                owner=user, book=obj
+            ).first()
+            return like.id if like else None
+        return None
+
     class Meta:
         model = Book
         fields = [
             'id', 'title', 'author', 'owner', 'is_owner', 'profile_id',
             'profile_image', 'created_at', 'updated_at',
-             'genre', 'language', 'ratings', 'image'
+             'genre', 'language', 'ratings', 'image', 'image_filter',
+             'like_id',
         ]
